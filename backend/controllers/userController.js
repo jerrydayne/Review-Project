d26e03db25dbfa7b6f5e4c1fdc4b462c1053a852
Jerry <jerrydaexplorer@gmail.com>
2022-11-02 21:58:09 +0100

    User login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,6 +52,21 @@ const registerUser = asynchHandler(async (req, res) => {
 // @routes POST  /api/reviews
 // @access Public
 const loginUser = asynchHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    //checking for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user credentials')
+    }
     res.json({ message: 'Login User'})
 })
 
